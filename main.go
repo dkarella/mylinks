9c2b6b192d5067da74a75755b5f511150d96ea3f
Daniel K. Arellano <arellanodanielkeith@gmail.com>
2020-09-25 22:14:21 -0700

    add ability to set new links

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,12 +11,21 @@ import (
 
 const fileName = "links.csv"
 
-var links map[string]string = make(map[string]string)
+type MyLinks struct {
+	file  *os.File
+	links map[string]string
+}
+
+var links MyLinks
 
 func main() {
-	load()
+	if err := links.Load(); err != nil {
+		log.Fatal(fmt.Errorf("failed to load links: %s", err))
+	}
+	defer links.Close()
 
 	http.HandleFunc("/health", healthCheckHandler)
+	http.HandleFunc("/setlink", setLinkHandler)
 	http.HandleFunc("/404", notFoundHandler)
 	http.HandleFunc("/", redirectHandler)
 
@@ -32,11 +42,33 @@ func main() {
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	redirect := "/404"
-	if l, ok := links[r.URL.Path[1:]]; ok {
+	if l, ok := links.links[r.URL.Path[1:]]; ok {
 		redirect = l
 	}
 
-	http.Redirect(w, r, redirect, 302)
+	http.Redirect(w, r, redirect, http.StatusTemporaryRedirect)
+}
+
+func setLinkHandler(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	key := q.Get("key")
+	value := q.Get("value")
+
+	if key == "" || value == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("key and value query parameters are required"))
+		return
+	}
+
+	if err := links.Set(key, value); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("unexpected error occurred: %s", err)))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,12 +81,19 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
 
-func load() {
-	file, err := os.Open(fileName)
+func (l *MyLinks) Load() error {
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_RDWR, os.ModeAppend)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	defer file.Close()
+	defer func() {
+		if err != nil {
+			file.Close()
+		}
+	}()
+
+	l.file = file
+	l.links = make(map[string]string)
 
 	scanner := bufio.NewScanner(file)
 
@@ -65,13 +104,27 @@ func load() {
 		tokens := strings.Split(line, ",")
 
 		if len(tokens) != 2 {
-			log.Fatalf("failed to load line: %s", line)
+			return fmt.Errorf("invalid line: %s", line)
 		}
 
-		links[tokens[0]] = tokens[1]
+		l.links[tokens[0]] = tokens[1]
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
+}
+
+func (l *MyLinks) Set(key, value string) error {
+	if _, err := l.file.WriteString(fmt.Sprintf("\n%s,%s", key, value)); err != nil {
+		return err
+	}
+	l.links[key] = value
+	return nil
+}
+
+func (l *MyLinks) Close() {
+	l.file.Close()
 }
